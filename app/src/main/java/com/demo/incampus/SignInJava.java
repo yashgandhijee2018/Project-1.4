d81a6be0104142a365f81f94a94394d7103a9f60
yashgandhijee2018 <yashgandhijee2018@gmail.com>
2020-03-26 22:46:22 +0530

    Update SignInJava.java
    updated sign in activity

--- app/src/main/java/com/demo/incampus/SignInJava.java
+++ app/src/main/java/com/demo/incampus/SignInJava.java
@@ -1,15 +1,22 @@
-package com.demo.incampus;
+ package com.demo.incampus;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.demo.incampus.Model.Register;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -23,12 +30,30 @@ import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.tasks.Task;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class SignInJava extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     GoogleSignInClient mGoogleSignInClient;
     GoogleApiClient mGoogleApiClient;
     SignInButton signInButtonGoogle;
+
+    private EditText email , password;
+    private Button mcontinue;
+    private ProgressBar mprogress;
+
+    String jwt_token="";
 
     int RC_SIGN_IN=0,RC_GET_AUTH_CODE=0;
 
@@ -42,8 +67,18 @@ public class SignInJava extends AppCompatActivity implements GoogleApiClient.OnC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_java);
 
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.pwd);
+        mcontinue = findViewById(R.id.contd);
+        mprogress = findViewById(R.id.progressBar);
+        mprogress.setVisibility(View.GONE);
+
+        final SharedPreferences preferences = getSharedPreferences("prefs", Context.MODE_PRIVATE);
+
+
 
         /*_______________________________________START_GOOGLE____________________________________________*/
+
         signInButtonGoogle=findViewById(R.id.login_ggl);
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -71,7 +106,170 @@ public class SignInJava extends AppCompatActivity implements GoogleApiClient.OnC
                 }
             }
         });
+        /*signInButtonGoogle=findViewById(R.id.login_ggl);
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestScopes(new Scope(Scopes.DRIVE_APPFOLDER))
+                .requestServerAuthCode(serverClientId)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        signInButtonGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.login_ggl:
+                        signIn();
+                        break;
+                    // ...
+                }
+            }
+        });
+         */
         /*__________________________________________END_GOOGLE______________________________________________*/
+
+
+        mcontinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(email.getText().toString().isEmpty() || password.getText().toString().isEmpty() /*|| preferences.getString("JWT" , "hello").equals("hello")*/)
+                {
+                    Toast.makeText(SignInJava.this, "Fields can't be Empty", Toast.LENGTH_SHORT).show();
+                }else
+                {
+                    mprogress.setVisibility(View.VISIBLE);
+                    mcontinue.setEnabled(false);
+                    Sign_In_Email(email.getText().toString() , password.getText().toString());
+                }
+
+            }
+        });
+
+    }
+
+
+    public void Sign_In_Email(String personGivenName,String personId)
+    {
+        SharedPreferences preferences = getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = preferences.edit();
+
+
+        Call<Register> call = RetrofitClient.getInstance().getApi().login(personGivenName,personId);
+
+        call.enqueue(new Callback<Register>() {
+
+            @Override
+            public void onResponse(Call<Register> call, Response<Register> response) {
+
+                if(response.isSuccessful()) {
+
+                    Register register = response.body();
+
+                    editor.putString("JWT", register.getAccessToken());
+                    editor.commit();
+
+                    Log.d("JWT" , register.getAccessToken());
+
+                    Toast.makeText(SignInJava.this, register.getAccessToken(), Toast.LENGTH_SHORT).show();
+
+                    mcontinue.setEnabled(true);
+                    mprogress.setVisibility(View.GONE);
+
+                    Intent intent = new Intent(SignInJava.this , PhoneNumberActivity.class);
+                    startActivity(intent);
+
+
+                    // Toast.makeText(SignInJava.this, register.getAccessToken(), Toast.LENGTH_SHORT).show();
+
+                }else
+                {
+                    mcontinue.setEnabled(true);
+                    mprogress.setVisibility(View.GONE);
+                    Toast.makeText(SignInJava.this, "User Does not exist", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Call<Register> call, Throwable t) {
+
+                mcontinue.setEnabled(true);
+                mprogress.setVisibility(View.GONE);
+
+                Toast.makeText(SignInJava.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        //API POST LOGIN USER
+        /*
+        Call<ResponseBody> login=RetrofitClient.getInstance().getApi().login("rajatmw1999","password");
+        login.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                try {
+                    if(response.isSuccessful())
+                    {
+
+
+                        ResponseBody register = response.body();
+
+                        Toast.makeText(SignInJava.this,register.string() ,Toast.LENGTH_SHORT).show();
+
+
+                    }else
+                    {
+                        Toast.makeText(SignInJava.this, "Response is not Successful !!", Toast.LENGTH_SHORT).show();
+                    }
+
+                }catch(Exception e)
+                {
+                    Toast.makeText(SignInJava.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+                try {
+
+
+                    ResponseBody responseBody = response.body();
+
+                    String dds= responseBody.string();
+
+                    JSONObject jsonObject=new JSONObject(dds);
+
+                    Toast.makeText(SignInJava.this, dds, Toast.LENGTH_SHORT).show();
+                    jwt_token = jsonObject.getString("accessToken");
+
+                    editor.putString("JWT", jwt_token);
+
+                    Log.i("access Token Server:",jwt_token);
+
+                } catch (IOException | JSONException e) {
+
+                    mcontinue.setEnabled(true);
+                    if( e instanceof NullPointerException){
+                        Toast.makeText(SignInJava.this, "Username does not exist", Toast.LENGTH_SHORT).show();
+                    }else{
+                    Toast.makeText(SignInJava.this, e.getMessage(), Toast.LENGTH_SHORT).show();}
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+
+        });  */
     }
 
     @Override
@@ -96,7 +294,7 @@ public class SignInJava extends AppCompatActivity implements GoogleApiClient.OnC
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
             // Signed in successfully, show authenticated UI.
-            Intent i=new Intent(this,OTPJava.class);
+            Intent i=new Intent(this,PhoneNumberActivity.class);
             startActivity(i);
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
